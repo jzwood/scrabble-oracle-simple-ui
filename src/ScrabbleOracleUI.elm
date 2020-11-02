4b60a9e3afe58e453501ed0841a37e703753bdb8
Jake Wood <jakew@captricity.com>
2020-11-02 10:35:27 -0500

    moved the FAQ to html

--- src/ScrabbleOracleUI.elm
+++ src/ScrabbleOracleUI.elm
@@ -1,10 +1,10 @@
 module ScrabbleOracleUI exposing (..)
 
 import Browser
-import Html exposing (Html, button, div, text, Attribute, Html, button, div, text, ul, li)
-import Html.Attributes exposing (style, class, tabindex)
-import Html.Events exposing (on, onClick, keyCode)
-import List.Extra exposing (getAt)
+import Html exposing (Html, button, div, text, Attribute, Html, button, div, span, text, ul, li, p, input, hr, h2, h3, h4, strong)
+import Html.Attributes exposing (style, class, tabindex, maxlength, type_)
+import Html.Events exposing (on, preventDefaultOn, onClick, keyCode)
+import List.Extra exposing (getAt, setAt)
 import Json.Decode as Json
 import List
 import Maybe
@@ -59,7 +59,8 @@ charToTile c =
     '2' -> Tile { letter = Nothing, color = "#ffc6ba" }
     '3' -> Tile { letter = Nothing, color = "#0093ab" }
     '4' -> Tile { letter = Nothing, color = "#ff6d4d" }
-    _ -> Tile { letter = Nothing, color = "#cdc3a5" }
+    '_' -> Tile { letter = Nothing, color = "#cdc3a5" }
+    _ -> Tile { letter = Just c, color = "#cdc3a5" }
 
 
 initBoard : Board
@@ -73,18 +74,63 @@ init =
   }
 
 -- UPDATE
+onKeyDown : (Int -> Msg) -> Attribute Msg
+onKeyDown letterConstructor =
+  preventDefaultOn "keydown" (Json.map alwaysPreventDefault (Json.map letterConstructor keyCode))
 
-onKeyUp : (Int -> msg) -> Attribute msg
-onKeyUp tagger =
-  on "keyup" (Json.map tagger keyCode)
+alwaysPreventDefault : msg -> (msg, Bool)
+alwaysPreventDefault msg = (msg, True)
 
-type Msg = SetDirection Direction | SetCursor Cursor | SetChar Char
+type Msg = SetDirection Direction | SetCursor Cursor | SetLetter Int
+
+updateTile : Char -> Tile -> Tile
+updateTile c (Tile ({ letter, color } as tile)) = Tile { tile | letter = (if c == ' ' then letter else Just c) }
+
+updateCursor : Cursor -> Direction -> Cursor
+updateCursor (Cursor (x, y)) dir =
+  case dir of
+    LeftToRight -> if y == 14
+      then
+        Cursor (remainderBy 15 (x + 1), 0)
+      else
+        Cursor (x, y + 1)
+    Down -> if x == 14
+      then
+        Cursor (0, remainderBy 15 (y + 1))
+      else
+        Cursor (x + 1, y)
+
+updateBoard : Cursor -> Char -> Board -> Board
+updateBoard (Cursor (x, y)) c (Board board) =
+  if Char.isAlpha c || c == ' ' then
+    case getAt x board of
+      Nothing -> Board board
+      Just rowX ->
+        case getAt y rowX of
+          Nothing -> Board board
+          Just tile ->
+            let
+              newRowX = setAt y (updateTile c tile) rowX
+              newBoard = setAt x newRowX board
+            in
+              Board newBoard
+  else Board board
 
 update : Msg -> Model -> Model
 update msg ({ board, cursorPos, direction } as model) =
   case msg of
     SetCursor cursor -> { model | cursorPos = cursor }
-    SetChar char -> { model | board = board}
+    SetLetter charCode ->
+      let
+          chr = Char.fromCode charCode
+      in
+        { model |
+          board = updateBoard cursorPos chr board,
+          cursorPos =
+            if Char.isAlpha chr || chr == ' '
+            then updateCursor cursorPos direction
+            else cursorPos
+        }
     SetDirection dir -> { model | direction = dir }
 
 -- VIEW
@@ -102,30 +148,84 @@ view ({board, cursorPos, direction} as model) =
         in
           div
             [ onClick (SetCursor (Cursor (x, y)))
+            , onKeyDown SetLetter
+            , tabindex x
+            , style "font-family" "'Work Sans', sans-serif"
+            , style "display" "flex"
+            , style "align-items" "center"
+            , style "justify-content" "center"
             , style "width" size
             , style "max-width" maxSize
             , style "height" size
             , style "max-height" maxSize
             , style "border" ("2px solid " ++ if hasCursor then "black" else "white")
-            , style "border-radius" (if hasCursor then "4px" else "0px")
+            , style "border-radius" (if hasCursor then "3px" else "0px")
             , style "box-sizing" "border-box"
-            , style "background-color" color
+            , style "background-color" (if letter == Nothing then color else "rgb(255, 242, 203)")
+            , style "font-size" "calc(14px + 1vw)"
             ] [ Maybe.withDefault ' ' letter |> String.fromChar |> text ]
   in
     div [ style "display" "flex"
         , style "flex-direction" "column"
         , style "align-items" "center"
+        , style "font-family" "sans-serif"
         ] (
-          div [ style "font-family" "'Work Sans', sans-serif"
-              , style "width" "100%"
-              , style "max-width" "667px"
-              , style "padding-bottom" "10px"
+          div [ style "width" "100%"
+              , style "max-width" "670px"
               ]
-              [
-                div [ style "font-size" "calc(16pt + 3vw)"
-                    ] [ "Scrabble Oracle" |> text ],
-                div [ style "font-size" "16pt"
-                    ] [ "Recreate your board by typing all tiles" |> text ]
+              [ span [ style "font-size" "calc(16pt + 3vw)"
+                     ] [ "Scrabble Oracle" |> text ]
+              , span [ style "font-size" "16px"
+                     , style "padding" "0 10px"
+                     , style "white-space" "nowrap"
+                     ] [ "find the highest scoring word" |> text ]
+              , ul [ style "font-size" "16pt"
+                   , style "padding-left" "0"
+                   , style "list-style" "none"
+                   ] [ li [ style "" "" ] [ "Recreate your board by clicking on any tile then start typing" |> text ]
+                     , li [] [ hr [] [] ]
+                     , li [ style "display" "flex"
+                          , style "justify-content" "space-between"
+                          , style "align-items" "center"
+                          , style "flex-wrap" "wrap"
+                          ] [ div [ style "margin-right" "15px" ]  [ span [] [ "RACK: " |> text ]
+                                    , input [ style "font-size" "16pt"
+                                      , style "margin" "7px 0"
+                                      , style "width" "13ch"
+                                      , type_ "text"
+                                      , maxlength 7
+                                      ] []
+                                    ]
+                           , div [] [ span [ style "margin-right" "10px" ] [ "DIRECTION:" |> text ]
+                                    , button [ style "background-color" "transparent"
+                                             , style "font-size" "16pt"
+                                             , style "outline" "none"
+                                             ] [ "RTL" |> text ]
+                               ]
+                           ]
+                    ]
               ] :: List.indexedMap (\i ts -> div
                 [ style "display" "flex"
-                ] (List.indexedMap (showTile i) ts)) (getBoard board))
+                ] (List.indexedMap (showTile i) ts)) (getBoard board) ++ [
+                  div [ style "width" "100%"
+                      , style "max-width" "670px"
+                      , style "font-size" "12pt"
+                      ] [ div [ style "display" "flex"
+                            , style "align-items" "center"
+                            , style "margin-top" "10px"
+                            ] [ span [ ] [ "EMAIL: " |> text ]
+                              , input [ style "margin" "7px 0"
+                                      , style "font-size" "16pt"
+                                      , style "width" "10ch"
+                                      , style "flex-grow" "1"
+                                      , type_ "text"
+                                      ] []
+                              , button [ style "background-color" "transparent"
+                                       , style "outline" "none"
+                                       , style "font-size" "16pt"
+                                       , style "margin-left" "7px"
+                                       ] [ "GET BEST WORD" |> text ]
+                              ]
+                            ]
+                        ]
+                      )
